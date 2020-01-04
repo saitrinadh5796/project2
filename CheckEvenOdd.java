7a5f9bfe9bd197775a555bd027c70450041effcc
saitrinadh5796 <59111224+saitrinadh5796@users.noreply.github.com>
2020-01-04 15:08:34 +0530

    Update CheckEvenOdd.java

--- CheckEvenOdd.java
+++ CheckEvenOdd.java
@@ -5,7 +5,6 @@ class CheckEvenOdd
   public static void main(String args[])
   {
     int num=2;
-    System.out.println("Enter an Integer number:");
     num = input.nextInt();
     if ( num % 2 == 0 )
         System.out.println("Entered number is even");
